c61d364adf654b1ace54b4d578d89b22fe211e47
FelipeEmerim <lip.emerim@gmail.com>
2021-04-07 17:59:15 -0300

    Fix parseInt checks
    Add null coalesce to within the function.

--- src/app.env.ts
+++ src/app.env.ts
@@ -4,13 +4,13 @@ dotenv.config();
 
 const env = {
   DB_HOST: process.env.DB_HOST ?? null,
-  DB_PORT: parseInt(process.env.DB_PORT, 10),
+  DB_PORT: parseInt(process.env.DB_PORT ?? '5432', 10),
   DB_USERNAME: process.env.DB_USERNAME ?? null,
   DB_PASSWORD: process.env.DB_PASSWORD ?? null,
   DB_DATABASE: process.env.DB_DATABASE ?? null,
   HOST: process.env.HOST ?? '0.0.0.0',
   PORT: process.env.PORT ?? 3000,
-  SALT: parseInt(process.env.SALT, 10) ?? 10,
+  SALT: parseInt(process.env.SALT ?? '10', 10),
 };
 
 export default env;
