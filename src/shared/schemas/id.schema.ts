6dda6ea25d03fb70b685c3cd384376f63082b267
FelipeEmerim <lip.emerim@gmail.com>
2021-04-30 20:42:54 -0300

    Check for UUID_v4

--- src/shared/schemas/id.schema.ts
+++ src/shared/schemas/id.schema.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
 
 export class IdSchema {
-  @IsUUID()
+  @IsUUID(4)
   @ApiProperty({
     example: '7ef3c0ba-03d7-49e4-8d83-7b7b6c8eb929',
     description: 'Indicate the id of the user',
